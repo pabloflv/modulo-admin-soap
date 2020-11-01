932836a2aa960cdb2eeeda0ddf65fbc74d0a000e
Gonzalo Berro <gberro@technisys.com>
2020-11-01 17:20:41 -0300

    fdgfdg

--- src/main/java/ar/com/unla/api/controllers/UsuarioController.java
+++ src/main/java/ar/com/unla/api/controllers/UsuarioController.java
@@ -18,6 +18,7 @@ import javax.validation.constraints.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -28,6 +29,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @Api(tags = "Usuario controller", description = "CRUD usuario")
+@CrossOrigin(origins = "*")
 @RestController
 @Validated
 @RequestMapping("/usuarios")
