672208db53003b0584e4a218f7a711b7a7592e1e
Gianni-Nicolas <nicolas.gianni@wolox.com.ar>
2020-11-06 14:01:14 -0300

    change length to image field into User model

--- src/main/java/ar/com/unla/api/models/database/Usuario.java
+++ src/main/java/ar/com/unla/api/models/database/Usuario.java
@@ -57,7 +57,7 @@ public class Usuario {
     @ApiModelProperty(notes = "password", required = true, position = 7)
     private String password;
 
-    @Column(length = 3000)
+    @Column(length = 65000)
     @ApiModelProperty(notes = "imagen", position = 8)
     private String imagen;
 
